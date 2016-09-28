8346efe2cb13024673ec9b4609ec8531e29a39a6
Gustavo Sobral <ghsobral@gmail.com>
2016-09-28 15:29:36 -0300

    Translate application models to pt-BR

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -11,21 +11,25 @@ describe Course do
     it 'does not accept empty name' do
       course.name = ''
       expect(course.valid?).to be false
+      expect(course.errors.messages[:name].join).to match('não pode ficar em branco')
     end
 
     it 'does not accept empty description' do
       course.description = ''
       expect(course.valid?).to be false
+      expect(course.errors.messages[:description].join).to match('não pode ficar em branco')
     end
 
     it 'does not accept too long name' do
       course.name = 'a' * 46
       expect(course.valid?).to be false
+      expect(course.errors.messages[:name].join).to match('é muito longo (máximo: 45 caracteres)')
     end
 
     it 'does not accept too long description' do
       course.description = 'a' * 46
       expect(course.valid?).to be false
+      expect(course.errors.messages[:description].join).to match('é muito longo (máximo: 45 caracteres)')
     end
   end
 end
--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -11,21 +11,25 @@ describe Student do
     it 'does not accept empty name' do
       student.name = ''
       expect(student.valid?).to be false
+      expect(student.errors.messages[:name].join).to match('não pode ficar em branco')
     end
 
     it 'does not accept empty register_number' do
       student.register_number = ''
       expect(student.valid?).to be false
+      expect(student.errors.messages[:register_number].join).to match('não pode ficar em branco')
     end
 
     it 'does not accept too long name' do
       student.name = 'a' * 46
       expect(student.valid?).to be false
+      expect(student.errors.messages[:name].join).to match('é muito longo (máximo: 45 caracteres)')
     end
 
     it 'does not accept too long register_number' do
       student.register_number = 'a' * 46
       expect(student.valid?).to be false
+      expect(student.errors.messages[:register_number].join).to match('é muito longo (máximo: 45 caracteres)')
     end
   end
 end
