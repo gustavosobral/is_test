f46838eb916e798c23a8aaca11b0040cca5344c2
Gustavo Sobral <ghsobral@gmail.com>
2016-09-29 09:02:44 -0300

    Create Student CRUD feature spec

---
+++ spec/features/students_crud_spec.rb
@@ -0,0 +1,29 @@
+feature 'I manage Student CRUD', type: :feature do
+  scenario 'creating, updating and destroying' do
+    student = FactoryGirl.build(:student)
+    visit students_path
+    expect(page).to have_no_content student.name
+
+    # Submit invalid
+    click_link 'Novo Estudante'
+    fill_in 'student_name', with: student.name
+    submit_form
+    expect(page).to have_content 'Número de registro não pode ficar em branco'
+
+    # Create
+    fill_in 'student_name', with: student.name
+    fill_in 'student_register_number', with: student.register_number
+    submit_form
+    expect(current_path).to eq(students_path)
+    expect(page).to have_content student.name
+
+    # Update
+    visit edit_student_path(1)
+    fill_in 'student_name', with: 'New student name'
+    submit_form
+    expect(page).to have_content 'New student name'
+
+    # Destroy
+    expect { click_link 'Excluir' }.to change(Student, :count).by(-1)
+  end
+end
